9d7c7910153601bbf5bf3cc59366e6a8309f8bdc
George Hany Milad <92086186+GeorgeHanyMilad@users.noreply.github.com>
2024-09-14 08:47:13 +0300

    Task

---
+++ Lesson (20) Homework Solution/Code.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Please enter the first number A ?" << endl;
+    int A;  cin >> A;
+    cout << "Please enter the second number B ?" << endl;
+    int B;  cin >> B;
+    cout << "A + B = " << A + B << endl;
+    cout << "A - B = " << A - B << endl;
+    cout << "A * B = " << A * B << endl;
+    cout << "A / B = " << A / B << endl;
+    cout << "A % B = " << A % B << endl;
+}
