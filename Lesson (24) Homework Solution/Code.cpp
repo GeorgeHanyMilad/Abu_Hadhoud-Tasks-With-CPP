979d44af322d618163330351e077ab421d0c0baf
George Hany Milad <92086186+GeorgeHanyMilad@users.noreply.github.com>
2024-09-14 09:22:06 +0300

    Task

---
+++ Lesson (24) Homework Solution/Code.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Please enter the first number A ?" << endl;
+    int A;  cin >> A;
+    cout << "Please enter the second number B ?" << endl;
+    int B;  cin >> B;
+    cout << A << " = " << B << " is " << (A == B) << endl;
+    cout << A << " != " << B << " is " << (A != B) << endl;
+    cout << A << " > " << B << " is " << (A > B) << endl;
+    cout << A << " < " << B << " is " << (A < B) << endl;
+    cout << A << " >= " << B << " is " << (A >= B) << endl;
+    cout << A << " <= " << B << " is " << (A <= B) << endl;
+}
