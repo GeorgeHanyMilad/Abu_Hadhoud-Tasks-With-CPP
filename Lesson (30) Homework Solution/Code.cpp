ccc4acf1a02b37e10e38cfc4c4507a8246ec1d88
George Hany Milad <92086186+GeorgeHanyMilad@users.noreply.github.com>
2024-09-17 08:26:52 +0300

    Task

--- Lesson (30) Homework Solution/Code.cpp
+++ Lesson (30) Homework Solution/Code.cpp
@@ -3,17 +3,21 @@
 using namespace std;
 
 int main() {
+    // Point #1
     string st1 = "43.22";
     double st1_d = stod(st1);
     float st1_f = stof(st1);
     int st1_i = stoi(st1);
 
+    // Point #2
     int N1 = 20;
     string st2 = to_string(N1);
 
+    // Point #3
     double N2 = 33.5;
     string st3 = to_string(N2);
 
+    // Point #4
     float N3 = 55.23;
     int num;
     string st4 = to_string(N3);
